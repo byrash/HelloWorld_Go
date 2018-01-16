960f6303e78a386375fee54d4e577234a85572fb
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2018-01-16 16:15:54 +1100

    evolved again

---
+++ Cards/deck.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// Create a new type of 'deck'
+// Which is slice of strings
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for index, card := range d {
+		fmt.Println(index, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	// return strings.Join(d, ", ")
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 -- Log error and return a call to newDeck()
+		// Option #2 -- Log error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
--- Cards/main.go
+++ Cards/main.go
@@ -1,17 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// card = "Five of Diamonds"
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// cards := deck{"Ace of Diamonds", newCard()}
+	cards := newDeck()
+	cards.shuffle()
+	// hand, remainingCards := deal(cards, 5)
+	// cards = append(cards, "Six of Spades")
+	// fmt.Println(cards)
+	// for index, card := range cards {
+	// 	fmt.Println(index, card)
+	// }
+	// cards.print()
+	// cards.saveToFile("my_cards")
+	// cards := newDeckFromFile("my_cards1")
+	cards.print()
 }
 
-func newCard() string {
-	return "Five Of Diamonds"
-}
+// func newCard() string {
+// 	return "Five Of Diamonds"
+// }
