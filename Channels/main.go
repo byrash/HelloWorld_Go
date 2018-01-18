b8332b6609df9f1d2ecf9c19ece5c114e0ba8ddd
Shivaji Byrapaneni <shivaji.byrapaneni@gmail.com>
2018-01-18 12:20:37 +1100

    Routines & Channels

---
+++ Channels/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	c := make(chan string)
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+	// This is same as above but more explicit and readable
+	// for l := range c {
+	// 	go checkLink(l, c)
+	// }
+	for l := range c {
+		//anon func called functiona literal in GO
+		go func(link string) {
+			time.Sleep(time.Second * 5)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down", err)
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up and running ")
+	c <- link
+}
